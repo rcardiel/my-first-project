f54a990ae4eb5d5641bc899801292664a7c06215
unknown <rcardielr@universidaddeleon.edu.mx>
2022-10-06 11:39:36 -0500

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { DbzModule } from './dbz/dbz.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DbzModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
